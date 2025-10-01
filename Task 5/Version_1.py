9164cb5525e8176488080a5d8ac6a2fb2a35699c
OsKaR85872 <48834885+oscar85872@users.noreply.github.com>
2025-10-01 21:56:34 +0300

    Delete Task 5 directory

--- Task 5/Version_1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-bk = pd.read_csv('books-en.csv', sep=';', encoding='latin-1')
-publishers = bk['Publisher'].unique()
-
-print("List of publishers without repetitions: ")
-print(publishers)
-
